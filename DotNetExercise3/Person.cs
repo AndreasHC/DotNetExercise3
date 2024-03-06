120d8707af507e813b9df539833977711a071c10
Andreas Hohwü-Christensen <andreas.hohwu@gmail.com>
2024-03-06 15:46:44 +0100

    Add project files.

---
+++ DotNetExercise3/Person.cs
@@ -0,0 +1,42 @@
+﻿
+namespace DotNetExercise3
+{
+    internal class Person
+    {
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        private string fName;
+        public string FName
+        {
+            get { return fName; }
+            set { fName = value; }
+        }
+
+        private string lName;
+        public string LName
+        {
+            get { return lName; }
+            set { lName = value; }
+        }
+
+        private int height;
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+        private int weight;
+        public int Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+    }
+}
